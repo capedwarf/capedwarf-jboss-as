ec214ca49b4ec0f1f4e9c01c4e35891e6baf8958
Ales Justin <ales.justin@gmail.com>
2013-11-04 12:20:01 +0100

    Add UT deps.

--- extension/src/main/java/org/jboss/as/capedwarf/deployment/CapedwarfDeploymentProcessor.java
+++ extension/src/main/java/org/jboss/as/capedwarf/deployment/CapedwarfDeploymentProcessor.java
@@ -79,6 +79,7 @@ public class CapedwarfDeploymentProcessor extends CapedwarfDeploymentUnitProcess
     private static final ModuleIdentifier BOUNCY_CASTLE_PKIX = ModuleIdentifier.create("org.bouncycastle.bcpkix");
     private static final ModuleIdentifier BOUNCY_CASTLE_PROV = ModuleIdentifier.create("org.bouncycastle.bcprov");
     private static final ModuleIdentifier COMMON_CORE = ModuleIdentifier.create("org.jboss.common-core");
+    private static final ModuleIdentifier UNDERTOW_CORE = ModuleIdentifier.create("io.undertow.core");
     private static final ModuleIdentifier UNDERTOW_SERVLET = ModuleIdentifier.create("io.undertow.servlet");
     private static final ModuleIdentifier MARSHALLING = ModuleIdentifier.create("org.jboss.marshalling");
     private static final ModuleIdentifier MARSHALLING_RIVER = ModuleIdentifier.create("org.jboss.marshalling.river");
@@ -104,7 +105,8 @@ public class CapedwarfDeploymentProcessor extends CapedwarfDeploymentUnitProcess
             BOUNCY_CASTLE_PKIX,
             BOUNCY_CASTLE_PROV,
             COMMON_CORE,
-        UNDERTOW_SERVLET,
+            UNDERTOW_CORE,
+            UNDERTOW_SERVLET,
             MARSHALLING,
             MARSHALLING_RIVER,
             JACKSON_CORE_ASL,
