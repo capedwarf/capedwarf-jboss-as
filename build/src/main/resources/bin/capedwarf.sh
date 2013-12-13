cec7dc096c91e4451ab8984848c0484edc041846
Ales Justin <ales.justin@gmail.com>
2013-12-13 23:36:02 +0100

    Fix .sh to work on OSX.

--- build/src/main/resources/bin/capedwarf.sh
+++ build/src/main/resources/bin/capedwarf.sh
@@ -2,14 +2,14 @@
 #if you need to set other boot options, please use standalone.sh
 
 DIRNAME=`dirname "$0"`
-REALPATH=`realpath "${DIRNAME}"`
+REALPATH=`cd "$DIRNAME/../bin"; pwd`
 
 #check if we need to run bytecode transformation
 if ! ls ${DIRNAME}/../modules/com/google/appengine/main/appengine-api-1.0-sdk-*-capedwarf* &> /dev/null; then
     ${DIRNAME}/capedwarf-bytecode.sh
 fi
 
-if [ -z "$1" ] then
+if [ -z "$1" ]; then
     ${DIRNAME}/standalone.sh -c standalone-capedwarf.xml
 else
     cd "$1"
